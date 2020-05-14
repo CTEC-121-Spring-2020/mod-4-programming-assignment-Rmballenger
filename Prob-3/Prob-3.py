74f7879de19371f0738b983a4109c8f0b5058217
Rmballenger <r.ballenger@students.clark.edu>
2020-05-14 13:44:09 -0700

    Finished Problem 3 started problem 4

--- Prob-3/Prob-3.py
+++ Prob-3/Prob-3.py
@@ -4,9 +4,42 @@
 
 # Robert Ballenger
 
+from graphics import *
 
 def main():
-    pass  # remove this line
+    win = GraphWin(title="Shooty Shooty Bow Target", height=400, width=400)
+    
+    # Little bit of shorthand, instead of writing "Point(200, 200)" for the middle of each circle, I just assigned it a variable.
+    center = Point(200, 200)
 
+    # First up we work the white circle. The 160 radius seems random, but it's beceause this was the last one I started with.
+    whiteC = Circle(center, 160)
+    whiteC.setFill("white")
+    whiteC.draw(win)
 
+    # Notice here how the black circl is 30 radius smaller... HMMM.
+    blackC = Circle(center, 120)
+    blackC.setFill("black")
+    blackC.draw(win)
+
+    # Blue, just like the rest.
+    blueC = Circle(center, 90)
+    blueC.setFill("blue")
+    blueC.draw(win)
+
+    # Almost done, we do red, which is 30 less on the radius.
+    redC = Circle(center, 60)
+    redC.setFill("red")
+    redC.draw(win)
+
+    # This last one draws a yellow circle with a radius of 30, which is where I actually started.
+    yellowC = Circle(center, 30)
+    yellowC.setFill("yellow")
+    yellowC.draw(win)
+
+    # This last command just keeps the window open until the user clicks, with text to accompany it.
+    Text(Point(200, 385), "Click again to quit.").draw(win)
+    win.getMouse()
+
+ 
 main()
--- Prob-4/Prob-4.py
+++ Prob-4/Prob-4.py
@@ -4,7 +4,42 @@
 
 # <YOUR NAME>
 
+from graphics import *
+
 def main():
-    pass # remove this line
+
+    win = GraphWin(title= "Buildy Buildy House House", height=400, width=400)
+    firstInstruction = Text(Point(200, 20), "Let's build a house!\n Click in two different areas to define corners").draw(win)
+
+    houseCorner1 = win.getMouse()
+    houseCorner2 = win.getMouse()
+
+    houseFrame = Rectangle(houseCorner1, houseCorner2)
+    houseFrame.setOutline("black")
+    houseFrame.setFill("beige")
+    firstInstruction.undraw()
+    houseFrame.draw(win)
+
+
+
+    input()
+    
+"""
+
+        Bonus = Text(Point(200, 20), "Bonus Round!").draw(win)
+    win.getMouse()
+    Bonus.undraw()
+    win = GraphWin(title="Clicky Clicky Square Square", height=400, width=400)
+    shape = Rectangle(Point(10, 40), Point(40, 10))
+    shape.setOutline("red")
+    shape.setFill("red")
+    shape.draw(win)
+
+    # The for loop gets slightly changed, instead of having c being the center of the shape, I assign it to the first point of the originally defined rectangle, which I made sure Point 1 was the bottom left for the assignment requirement. The math is still the same, however I added a variable called 'shapeCopy' so when the clicks happen it creates a new one and moves/draws it.
+    for i in range(5):
+        p = win.getMouse()
+        c = shape.getP1()
+"""
+
 
 main()
