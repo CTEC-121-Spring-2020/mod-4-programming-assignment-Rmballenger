5e55dd4e86924ea40cfecf0bfff935721ff59ded
Rmballenger <R.ballenger@students.clark.edu>
2020-05-14 18:14:00 -0700

    Finished Problem 4

--- Prob-4/Prob-4.py
+++ Prob-4/Prob-4.py
@@ -6,10 +6,12 @@
 
 from graphics import *
 
+
 def main():
 
-    win = GraphWin(title= "Buildy Buildy House House", height=400, width=400)
-    firstInstruction = Text(Point(200, 20), "Let's build a house!\n Click in two different areas to define corners").draw(win)
+    win = GraphWin(title="Buildy Buildy House House", height=400, width=400)
+    instruction1 = Text(Point(
+        200, 20), "Let's build a house!\nClick in two different areas to define corners").draw(win)
 
     houseCorner1 = win.getMouse()
     houseCorner2 = win.getMouse()
@@ -17,29 +19,101 @@ def main():
     houseFrame = Rectangle(houseCorner1, houseCorner2)
     houseFrame.setOutline("black")
     houseFrame.setFill("beige")
-    firstInstruction.undraw()
+    instruction1.undraw()
     houseFrame.draw(win)
 
+    houseX1 = houseCorner1.getX()
+    houseX2 = houseCorner2.getX()
 
+    houseY1 = houseCorner1.getY()
+    houseY2 = houseCorner2.getY()
+
+    if houseY1 > houseY2:
+        bottomLeft = houseY1
+        topRight = houseY2
+    else:
+        bottomLeft = houseY2
+        topRight = houseY1
+
+    if houseX1 < houseX2:
+        topLeft = houseX1
+        bottomRight = houseX2
+    else:
+        topLeft = houseX2
+        bottomRight = houseX1
+
+    frameTopLeft = Point(topLeft, topRight)
+    frameTopRight = Point(bottomRight, topRight)
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    instruction2 = Text(Point(
+        200, 20), "Next, click where you would like\nthe center of the top of the door").draw(win)
+
+    clickDoor = win.getMouse()
+    doorX = clickDoor.getX()
+    doorY = clickDoor.getY()
+
+    doorHeight = bottomLeft
+    '''
+    if houseY1 > houseY2:
+        doorHeight = houseY1
+    else:
+        doorHeight = houseY2
+    '''
+    doorWidth = (((houseX2 - houseX1)/5)/2)
+    doorFrameX1 = doorX - doorWidth
+    doorFrameX2 = doorX + doorWidth
+
+    doorFrame = Rectangle(Point(doorFrameX1, doorY),
+                          Point(doorFrameX2, doorHeight))
+
+    doorFrame.setOutline("black")
+    doorFrame.setFill("brown")
+    doorFrame.draw(win)
+    instruction2.undraw()
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    instruction3 = Text(Point(
+        200, 20), "Why don't we put up a nice little window.\nClick where you would like one.").draw(win)
+
+    windowCenter = win.getMouse()
+    windowclickX = windowCenter.getX()
+    windowclickY = windowCenter.getY()
+
+    windowWidth = doorWidth/2
+    windowX1 = windowclickX - windowWidth
+    windowY1 = windowclickY - windowWidth
+    windowX2 = windowclickX + windowWidth
+    windowY2 = windowclickY + windowWidth
+
+    windowFrame = Rectangle(Point(windowX1, windowY1),
+                            Point(windowX2, windowY2))
+
+    windowFrame.setOutline("black")
+    windowFrame.setFill("blue")
+    windowFrame.draw(win)
+    instruction3.undraw()
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    instruction4 = Text(Point(
+        200, 20), "Last, please click where you would like\nthe peak of the roof to be.").draw(win)
+
+    housePeak = win.getMouse()
+
+    p1 = frameTopLeft
+    p2 = housePeak
+    p3 = frameTopRight
+
+    roofFrame = Polygon(p1, p2, p3)
+
+    instruction4.undraw()
+    roofFrame.setOutline("black")
+    roofFrame.setFill("red")
+    roofFrame.draw(win)
 
     input()
-    
-"""
-
-        Bonus = Text(Point(200, 20), "Bonus Round!").draw(win)
-    win.getMouse()
-    Bonus.undraw()
-    win = GraphWin(title="Clicky Clicky Square Square", height=400, width=400)
-    shape = Rectangle(Point(10, 40), Point(40, 10))
-    shape.setOutline("red")
-    shape.setFill("red")
-    shape.draw(win)
-
-    # The for loop gets slightly changed, instead of having c being the center of the shape, I assign it to the first point of the originally defined rectangle, which I made sure Point 1 was the bottom left for the assignment requirement. The math is still the same, however I added a variable called 'shapeCopy' so when the clicks happen it creates a new one and moves/draws it.
-    for i in range(5):
-        p = win.getMouse()
-        c = shape.getP1()
-"""
 
 
 main()
