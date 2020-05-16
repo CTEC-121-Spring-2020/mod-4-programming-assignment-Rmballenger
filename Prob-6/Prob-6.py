ce260ca013668d55c7f57a8c04b91638cab0c9ea
Rmballenger <r.ballenger@students.clark.edu>
2020-05-15 17:06:42 -0700

    Finished Assignment

--- Prob-6/Prob-6.py
+++ Prob-6/Prob-6.py
@@ -9,24 +9,146 @@ from graphics import *
 
 def main():
     
-    win = GraphWin("Legos",1100,1000)
-    # draw basic brick
+    win = GraphWin("Steppy Steppy Owe Owe",1100,1000)
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    # blue brick
     blueBrick = Rectangle(Point(10,280), Point(510, 100))
     blueBrick.setFill("blue")
     blueBrick.setOutline("black")
     blueBrick.setWidth(5)
     blueBrick.draw(win)
-    # draw first nib
+
+    # blue nibs
     blueBrickNib = Rectangle(Point(35, 100), Point(85, 80))
     blueBrickNib.setFill("blue")
     blueBrickNib.setOutline("black")
     blueBrickNib.setWidth(5)
     blueBrickNib.draw(win)
-    # test
+    
+    # dupeing nibs
     for i in range(1, 5):
         blueBrickNextNib = blueBrickNib.clone()
         blueBrickNextNib.move(100 *i, 0)
         blueBrickNextNib.draw(win)
-    input()
+
+ #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    # green brick
+    greenBrick = blueBrick.clone()
+    greenBrick.setFill("green")
+    greenBrick.move(575, 0)
+    greenBrick.draw(win)
+
+    # green nibs
+    greenBrickNib = blueBrickNib.clone()
+    greenBrickNib.setFill("green")
+    greenBrickNib.move(575, 0)
+    greenBrickNib.draw(win)
+
+    # dupeing nibs
+    for i in range(1, 5):
+        greenBrickNextNib = greenBrickNib.clone()
+        greenBrickNextNib.move(100 *i, 0)
+        greenBrickNextNib.draw(win)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    # yellow brick
+    yellowBrick = blueBrick.clone()
+    yellowBrick.setFill("yellow")
+    yellowBrick.move(0, 300)
+    yellowBrick.draw(win)
+
+    # yellow nibs
+    yellowBrickNib = blueBrickNib.clone()
+    yellowBrickNib.setFill("yellow")
+    yellowBrickNib.move(0, 300)
+    yellowBrickNib.draw(win)
+
+    # dupeing nibs
+    for i in range(1, 5):
+        yellowBrickNextNib = yellowBrickNib.clone()
+        yellowBrickNextNib.move(100 *i, 0)
+        yellowBrickNextNib.draw(win)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    # red brick
+    redBrick = greenBrick.clone()
+    redBrick.setFill("red")
+    redBrick.move(0, 300)
+    redBrick.draw(win)
+
+    # red nibs
+    redBrickNib = greenBrickNib.clone()
+    redBrickNib.setFill("red")
+    redBrickNib.move(0, 300)
+    redBrickNib.draw(win)
+
+    # dupeing nibs
+    for i in range(1, 5):
+        redBrickNextNib = redBrickNib.clone()
+        redBrickNextNib.move(100 *i, 0)
+        redBrickNextNib.draw(win)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    # light blue brick
+    lightBlueBrick = yellowBrick.clone()
+    lightBlueBrick.setFill("light blue")
+    lightBlueBrick.move(0, 300)
+    lightBlueBrick.draw(win)
+
+    # lightBlue nibs
+    lightBlueBrickNib = yellowBrickNib.clone()
+    lightBlueBrickNib.setFill("light blue")
+    lightBlueBrickNib.move(0, 300)
+    lightBlueBrickNib.draw(win)
+
+    # dupeing nibs
+    for i in range(1, 5):
+        lightBlueBrickNextNib = lightBlueBrickNib.clone()
+        lightBlueBrickNextNib.move(100 *i, 0)
+        lightBlueBrickNextNib.draw(win)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    # black brick
+    blackBrick = redBrick.clone()
+    blackBrick.setFill("black")
+    blackBrick.move(0, 300)
+    blackBrick.draw(win)
+
+    # the inner red for the black brick
+    innerBlackBrick= Rectangle(Point(590, 705), Point(1080, 875))
+    innerBlackBrick.setOutline("dark red")
+    innerBlackBrick.draw(win)
+
+    # black brick
+    blackBrickNib = redBrickNib.clone()
+    blackBrickNib.setFill("black")
+    blackBrickNib.move(0, 300)
+    blackBrickNib.draw(win)
+
+    # inner red for the nib
+    innerBlackBrickNib = Rectangle(Point(610, 682), Point(660, 699))
+    innerBlackBrickNib.setOutline("dark red")
+    innerBlackBrickNib.draw(win)
+
+    # dupeing nibs
+    
+    for i in range(1, 5):
+        blackBrickNextNib = blackBrickNib.clone()
+        innerBlackBrickNextNib = innerBlackBrickNib.clone()
+        blackBrickNextNib.move(100 *i, 0)
+        innerBlackBrickNextNib.move(100 * i, 0)
+        blackBrickNextNib.draw(win)
+        innerBlackBrickNextNib.draw(win)
+    
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    win.getMouse()
 
 main()
--- Prob-7/Prob-7.py
+++ Prob-7/Prob-7.py
@@ -2,10 +2,99 @@
 #   Programming Assignment 6
 #       Prob-7.py
 
-# <YOUR NAME>
+# Robert Ballenger
+
+from graphics import *
+
 
 def main():
-    pass # remove this line 
+
+    win = GraphWin(title="Face Face McFace Face", height=800, width=800)
+
+    #~~~~~~~~~~~~~~~~~~~BASE~~~~~~~~~~~~~~~~~~~~~~~
+
+    faceBase = Oval(Point(60.0, 3.0), Point(757.0, 787.0))
+    faceBase.setOutline("black")
+    faceBase.setFill("light grey")
+    faceBase.draw(win)
+
+    #~~~~~~~~~~~~~~~~~~~MOUTH~~~~~~~~~~~~~~~~~~~~~~~
+
+    faceMouth = Polygon(Point(108, 450), Point(290, 555), Point(523, 555), Point(705, 450), Point(535, 745), Point(290, 745))
+    faceMouth.setOutline("black")
+    faceMouth.setFill("dark red")
+    faceMouth.draw(win)
+
+    #~~~~~~~~~~~~~~~~~~~TEETH~~~~~~~~~~~~~~~~~~~~~~~
+
+    faceTooth1 = Polygon(Point(290, 555), Point(320.0, 696.0), Point(357.0, 555.0))
+    faceTooth1.setOutline("black")
+    faceTooth1.setFill("light grey")
+    faceTooth1.draw(win)
+
+    faceTooth2 = faceTooth1.clone()
+    faceTooth2.move(165, 0)
+    faceTooth2.draw(win)
+
+    faceTooth3 = Polygon(Point(290.0, 745.0), Point(305.0, 720.0), Point(375.0, 720.0), Point(390.0, 745.0))
+    faceTooth3.setOutline("black")
+    faceTooth3.setFill("light grey")
+    faceTooth3.draw(win)
+
+    faceTooth4 = faceTooth3.clone()
+    faceTooth4.move(145, 0)
+    faceTooth4.draw(win)
+
+    #~~~~~~~~~~~~~~~~~~~NOSE~~~~~~~~~~~~~~~~~~~~~~~~
+
+    faceNose = Polygon(Point(409.0, 321.0), Point(364.0, 500.0), Point(453.0, 500))
+    faceNose.setOutline("black")
+    faceNose.setFill("dark grey")
+    faceNose.draw(win)
+
+    #~~~~~~~~~~~~~~~~~~~EYES~~~~~~~~~~~~~~~~~~~~~~~~
+
+    faceEye1 = Circle(Point(270, 235), 90)
+    faceEye1.setOutline("black")
+    faceEye1.setFill("white")
+    faceEye1.draw(win)
+
+    faceInnerEye1 = Circle(Point(270, 235), 20)
+    faceInnerEye1.setFill("black")
+    faceInnerEye1.draw(win)
+
+    faceEye2 = faceEye1.clone()
+    faceEye2.move(275, 0)
+    faceEye2.draw(win)
+
+    faceInnerEye2 = faceInnerEye1.clone()
+    faceInnerEye2.move(275, 0)
+    faceInnerEye2.draw(win)
+
+    #~~~~~~~~~~~~~~~~~~~EARS~~~~~~~~~~~~~~~~~~~~~~~~
+
+    faceEar1 = Oval(Point(14.0, 174.0), Point(104.0, 436.0))
+    faceEar1.setOutline("black")
+    faceEar1.setFill("darkgrey")
+    faceEar1.draw(win)
+
+    faceEar2 = faceEar1.clone()
+    faceEar2.move(675, 0)
+    faceEar2.draw(win)
+
+    #~~~~~~~~~~~~~~~~~~~HAIR~~~~~~~~~~~~~~~~~~~~~~~~
+
+    faceHair = Polygon(Point(109.0, 0.0), Point(11.0, 181.0), Point(190.0, 123.0), Point(154.0, 256.0), Point(505.0, 87.0), Point(732.0, 76.0), Point(730, 0))
+    faceHair.setOutline("orange")
+    faceHair.setFill("dark orange")
+    faceHair.draw(win)
+   
+    #~~~~~~~~~~~~~~~~~~TRASH~~~~~~~~~~~~~~~~~~~~~~~~
+    
+
+    Text(Point(400, 10), "Click again to quit...").draw(win)
+    win.getMouse()
+
 
 
 main()
